730641e8a81333b8780ea80f8a31bd981178eba9
IvanKostishin <112808887+IvanKostishin@users.noreply.github.com>
2023-02-12 20:02:29 +0200

    js

---
+++ js/01-gallery.js
@@ -0,0 +1,42 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryItemMarkup = createGallery(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createGallery(items) {
+    return items.map(item => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}" onclick="return false;" rel="noreferrer noopener">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`).join('');
+}
+
+galleryContainer.innerHTML = galleryItemMarkup;
+
+galleryContainer.addEventListener('click', onParentClick);
+
+function onParentClick(evt) {
+    evt.preventDefault();
+    
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+    
+    const instance = basicLightbox.create(`
+        <img src= "${evt.target.dataset.source}">`,  {
+        onShow: () => document.addEventListener("keydown", function(event){if (event.code === "Escape") {
+         instance.close();
+    }}),
+        onClose: () => document.removeEventListener("keydown", function(event){if (event.code === "Escape") {
+         instance.close();
+    }}),
+    }
+)
+    instance.show(); 
+}
